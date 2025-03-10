bfd30607932c931ee3fa9fb372377c1850c8d38b
Ananya Kumari <142160654+ananya240903@users.noreply.github.com>
2025-03-11 01:33:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 import pandas as pd
 import numpy as np
+import os
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
@@ -8,7 +9,12 @@ from sklearn.preprocessing import LabelEncoder
 app = Flask(__name__)
 
 # Load and preprocess the dataset
-df = pd.read_csv("signal_db3.csv")  # Ensure this file is uploaded to GitHub
+# Get the current directory of the script
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+CSV_FILE_PATH = os.path.join(BASE_DIR, "signal_db3.csv")
+
+df = pd.read_csv(CSV_FILE_PATH)
+  # Ensure this file is uploaded to GitHub
 df.columns = df.columns.str.strip()
 
 # Encode categorical data
